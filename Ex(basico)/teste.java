4359c0d64ed880d8b8c47cfbc0373f0a78f35932
elcio <elcioferreira398@gmail.com>
2025-02-14 22:59:09 +0000

    aula basica 2

---
+++ Ex(basico)/teste.java
@@ -0,0 +1,25 @@
+public class teste{
+    public static void main(String[] args) throws Exception{
+        System.out.println("hello word ");
+
+        String nome = "Parabens para vc";
+        int idade = 2;
+        double altura = 1.60;
+        double peso = 70.0;
+        for(int i=0 ; i<idade; i++){
+
+            System.out.println(nome);
+        }
+
+        double resultIMC = peso/(Math.pow(altura, 2));
+         System.out.println(resultIMC);
+
+         
+        
+          
+        
+
+    }
+
+    
+}
